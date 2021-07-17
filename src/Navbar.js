5c93261e931d55402710df64c6d25d63579fbb8f
iamAravinda <acfernando1999@gmail.com>
2021-07-17 22:13:32 +0530

    navbar created

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faFacebook,faInstagram,faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { faBars,faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 const Navbar = () => {
+    const [click,setClick]=useState(false);
+    const handleClick= () => setClick(!click);
     return ( 
             <nav>
-                <div className="links">
-                    <h2>Spicy Hut</h2>
-                    <ul>
+                <h2>Spicy Hut</h2>
+                    <ul className={click ?"links active" : "links"}>
                         <li><a href="/">Home</a></li>
                         <li><a href="/">Menu</a></li>
                         <li><a href="/">About Us</a></li>
                         <li><a href="/">Contact Us</a></li>
                     </ul>
-                </div>
-                <div className="socialMedia-icons">
-                   <button><FontAwesomeIcon icon={faFacebook}/></button>
-                   <button><FontAwesomeIcon icon={faInstagram}/></button>
-                   <button><FontAwesomeIcon icon={faTwitter}/></button>
+                    <div className="icons"> 
+                        <button ><FontAwesomeIcon icon={faFacebook}/></button>
+                        <button><FontAwesomeIcon icon={faInstagram}/></button>
+                        <button><FontAwesomeIcon icon={faTwitter}/></button>
+                    </div>
+                <div className="menu" onClick={handleClick}>
+                        <button><FontAwesomeIcon icon={click ? faTimes: faBars }/></button>
                 </div>
             </nav>
      );
 }
- 
+
 export default Navbar;
