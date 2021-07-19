e691c859e99ea803abd2a9f7463986694c8a4382
iamAravinda <acfernando1999@gmail.com>
2021-07-19 23:01:25 +0530

    add text to service area

--- src/services.js
+++ src/services.js
@@ -4,13 +4,20 @@ const services = () => {
     return ( 
         <div className="services">
             <h1 className="title">Services</h1>
+            <hr />
             <div className="take">
-                <h1 className="topic">Takeaway</h1>
+                <div className="text">
+                    <h1>Takeaway</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ducimus natus vitae architecto error, corrupti dolore eveniet sunt delectus quidem eius illo, fuga ad alias nisi, tenetur voluptatum! Tempore quam quos sit explicabo dignissimos provident?</p>
+                </div>
                 <img src={img1} alt="" />
             </div>
             <div className="parties">
                 <img src={img2} alt="" />
-                <h1 className="topic">Pre-Orders</h1>
+                <div className="text">
+                    <h1>Pre-Orders</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quasi fuga quo! Repellendus fuga eligendi aliquid. Nemo similique atque ad vel molestias, perferendis accusantium. Soluta, labore? Delectus nihil repudiandae hic similique aut explicabo, id quod?</p>
+                </div>
             </div>
         </div>
      );
