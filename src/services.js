fd8ea722f28b0f97d6f2309932b83b49715b1b83
iamAravinda <acfernando1999@gmail.com>
2021-07-21 01:30:36 +0530

    completed service section

--- src/services.js
+++ src/services.js
@@ -8,17 +8,18 @@ const services = () => {
             <hr />
             <div className="take">
                 <div className="text">
-                    <h1>Takeaway</h1>
+                <h1>Takeaway</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ducimus natus vitae architecto error, corrupti dolore eveniet sunt delectus quidem eius illo, fuga ad alias nisi, tenetur voluptatum! Tempore quam quos sit explicabo dignissimos provident?</p>
                 </div>
-                <ImageSlide slides={SliderData}/>
+                <ImageSlide slides={SliderData} className="slide1"/>
             </div>
+            <hr />
             <div className="parties">
-                <ImageSlide slides={SliderData1}/>
                 <div className="text">
-                    <h1>Pre-Orders</h1>
+                    <h1>Parties</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quasi fuga quo! Repellendus fuga eligendi aliquid. Nemo similique atque ad vel molestias, perferendis accusantium. Soluta, labore? Delectus nihil repudiandae hic similique aut explicabo, id quod?</p>
                 </div>
+                <ImageSlide slides={SliderData1} className="slide2"/>
             </div>
         </div>
      );
