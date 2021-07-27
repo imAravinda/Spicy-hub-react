c171ece9f82a81f127fa672e49ce267d2ebb0604
iamAravinda <acfernando1999@gmail.com>
2021-07-27 13:40:00 +0530

    struct the menu page

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,153 @@
 const Menu = () => {
     return ( 
         <div className="menu">
-            <h1>Menu</h1>
+            <div className="leftSide">
+                <h1>Menu <sup>Spicy Hut</sup> <hr /></h1>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="rightSide">
+                <div className="foods">
+                <h2>Main</h2>
+                    <hr />
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="foods">
+                <h2>Noodles</h2>
+                    <hr />
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="foods">
+                <h2>Soups</h2>
+                    <hr />
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="foods">
+                <h2>Pizza</h2>
+                    <hr />
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="foods">
+                <h2>Desserts</h2>
+                    <hr />
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="food">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                        <div className="cost">
+                            <p>Rs.1000/-</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
- 
 export default Menu;
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,20 +1,23 @@
-import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const SignIn = () => {
 
     return ( 
         <div className="Sign">
-            <form action="" className="SignIn" name="SignIn">
-                <h3>Sign In</h3>
+            <form action="" className="SignUp" name="SignIn">
+                <h3>Register</h3>
                 <hr />
                 <label htmlFor="UserName">User Name</label><br />
                 <FontAwesomeIcon icon={faUser} className="logI"/>
-                <input type="text" name="username" id ="username" value="" required placeholder="User Name"/><br />
+                <input type="text" name="username" id ="username"  required placeholder="User Name"/><br />
+                <label htmlFor="Email">Email</label><br />
+                <FontAwesomeIcon icon={faEnvelope} className="logI"/>
+                <input type="email" name="email" id ="email"  required placeholder="Mail@gmail.com"/><br />
                 <label htmlFor="">Password</label><br />
                 <FontAwesomeIcon icon={faLock} className="logI"/>
                 <input type="password" name="password" id="password" required placeholder="Password"/><br />
                 <button className="Log">Log In</button><br />
-                <span id="signup">Don't have an account ? <a href="#"> Sign Up </a></span>
+                <span id="signup">Already have an account ? <a href="#"> Sign In </a></span>
             </form>
         </div>
      );
